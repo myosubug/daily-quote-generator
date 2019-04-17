4116e3d00d87075ca0305a031def0e08711c97e2
myosubug <seanoh.dev@gmail.com>
2019-04-17 05:03:21 -0600

    updated https

--- docs/quote.js
+++ docs/quote.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var html = "";
     var arr = [];
 
-    $.getJSON("http://quotes.rest/qod.json", function(a) {
+    $.getJSON("https://quotes.rest/qod.json", function(a) {
        console.log(a.contents.quotes[0]);
        
       html = "<h5><blockquote id='content'>"+a.contents.quotes[0].quote+"</blockquote><p id='author'> by "+a.contents.quotes[0].author+"<br><br> Today's date: "+a.contents.quotes[0].date+ "</p></h5>";
