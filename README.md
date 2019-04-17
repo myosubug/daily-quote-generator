37b738c9d264c1a64e985826eda3e9527f21e5de
myosubug <seanoh.dev@gmail.com>
2019-04-17 04:57:55 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# daily quote generator
+
+This is a simple to-do list that can add/remove to-dos.
+
+## Live example
+
+* 
+
+## Prerequisites
+
+* any web browser
+
+## Installing
+
+No installation is required.
+
+## Built With
+
+* JQuery
+* html
+* css
+* bootstrap
+
---
+++ docs/src/js/quote.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var html = "";
+    var arr = [];
+
+    $.getJSON("http://quotes.rest/qod.json", function(a) {
+       console.log(a.contents.quotes[0]);
+       
+      html = "<h5><blockquote id='content'>"+a.contents.quotes[0].quote+"</blockquote><p id='author'> by "+a.contents.quotes[0].author+"<br><br> Today's date: "+a.contents.quotes[0].date+ "</p></h5>";
+          
+      });  
+      
+    $("#getQuote").on("click", function(){
+      $(".quote").html(html);
+     });
+        
+    
+    });                           
+    
